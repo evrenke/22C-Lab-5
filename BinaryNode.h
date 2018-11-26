7ed0105ad6bb1391199b0248f5e4cffdc631e010
jasonhagene <43765704+jasonhagene@users.noreply.github.com>
2018-11-25 21:58:42 -0800

    Add files via upload

---
+++ BinaryNode.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Node.h"
+template<class ot>
+class BinaryNode : protected Node<ot> {
+private:
+	BinaryNode<ot>* rightChild;
+	BinaryNode<ot>* leftChild;
+public:
+	BinaryNode() {
+		setData(nullptr);
+		setLeftChild(nullptr);
+		rightChild = NULL;
+	}
+	BinaryNode(ot object) {
+		this->setData(&object);
+	}
+
+	BinaryNode<ot>* getLeftChild() { return leftChild; }
+	void setLeftChild(BinaryNode<ot>* in) { leftChild = in; }
+	BinaryNode<ot>* getRightChild() { return rightChild; }
+	void setRightChild(BinaryNode<ot>* in) { rightChild = in; }
+	ot* contents() { return this->getObj(); }
+	void setInfo(ot* in) { this->setData(in); }
+	
+	//Deletes all nodes below to prevent memory leaks. Deleting the root will delete the entire tree.
+	//If removing a single node from the tree is desired, use the remove function within the BST class instead.
+	~BinaryNode() {
+		delete this->getObj();
+		delete rightChild;
+		delete getLeftChild();
+	}
+};
